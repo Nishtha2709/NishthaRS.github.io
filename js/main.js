89481885265c676d92d9a79fc63605ff8ecba6dd
Nishtha2709 <nish279rs@gmail.com>
2021-07-18 00:40:55 +0530

    made reponsive

--- js/main.js
+++ js/main.js
@@ -107,3 +107,11 @@
         
         })
 })();
+
+
+window.addEventListener("load",()=>{
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(()=>{
+        document.querySelector(".preloader").style.display="none";
+    },600)
+})
